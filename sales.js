894b8bb851690af284135bd2fa59197b19b19038
Chris-PC\Chris <cdubois2@mail.csuchico.edu>
2016-11-02 19:12:01 -0700

    v 0.2

--- sales.js
+++ sales.js
@@ -17,15 +17,8 @@ function addItem()
     dollars = asCurrency(runningTotal);
     document.getElementById("subtotal").innerHTML = dollars;
     document.getElementById("price").value = " ";
-    setCookie("preTax",runningTotal,0);
+    setCookie("preTax",runningTotal,1);
   }
-  // update newItem to its value cast as a number
-  // update runningTotal to be its value plus newItem
-  // create a variable called dollars
-  // call asCurrency() by with the value of runningTotal and assign the return value to dollars
-  // update the innerHTML of the span with the id "subtotal" to be dollars
-  // update the value of the input with the id "price" to be an empty string
-  // update a cookie called "preTax" with the value of runningTotal
 }
 
 function calculateReceipt(){
